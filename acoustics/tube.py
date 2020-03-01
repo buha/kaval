e5c8d8c336df09bdb7fac57393c29108a702e9c4
buha <mail@dari.bz>
2020-03-02 00:37:00 +0200

    fr / fb could be an instrument constant !!!!
    This seems quite likely at this point but needs more investigation.

--- acoustics/tube.py
+++ acoustics/tube.py
@@ -6,6 +6,7 @@ class Tube:
         self.dle = config['dle']
         self.holes = config['holes']
         self.c = 346 * 1000 # TODO: move in config
+        self.dleratio = 1.05
 
     def analyze(self, registers=3):
         # On the Radiation of Sound from an Unflanged Circular Pipe
@@ -18,23 +19,30 @@ class Tube:
             # player's lips but is somehow vibrating openly, so apply the
             # end correction for both ends.
             self.dle = self.dlt
+        else:
+            self.dle = (self.dleratio - (self.lt + self.dlt) / (self.lt + 2 * self.dlt)) * (self.lt + 2 * self.dlt)
 
         # Cris Forster - Musical Mathematics, Eq. 8.20
         lengths = [self.dle + self.lt + self.dlt]
+
+        dle = self.dle
         
         # compute the acoustic lengths of air columns at each hole
         for i, h in enumerate(self.holes):
             # Cris Forster - Musical Mathematics, Eq. 8.14
             LBh = (self.gt + h[1]) * (self.dt / h[1]) ** 2 - 0.45 * self.dt
             # Cris Forster - Musical Mathematics, Eq. 8.23
-            LL = h[0] + self.dle
+            LL = h[0] + dle
             # Cris Forster - Musical Mathematics, Eq. 8.24 and 8.25
             LT = lengths[i]
             # Cris Forster - Musical Mathematics, Eq. 8.22
             # based on Nederveen, Acoustical Aspects of Woodwind instruments, Eq. 32.14
             LA = LL + LBh * (LT - LL) / (LT - LL + LBh)
 
+            dle = (self.dleratio - (LA + self.dlt) / (LA + 2 * self.dlt)) * (LA + 2 * self.dlt)
+
             lengths.append(LA)
+            print("Hole {} embouchure correction {}".format(i, dle))
         
         frequencies = dict()
         for n in range(1, registers + 1):
